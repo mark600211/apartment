686f0837f6ea3632a27c4dab53d561bf917cc4f8
mark <marx.design@icloud.com>
2020-07-26 21:03:16 +0500

    add liked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatCardModule } from '@angular/material/card'
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { ApartmentsListComponent } from './apartments-list.component';
 import { ApartmentComponent } from './apartment.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ApartmentsListComponent,
-    ApartmentComponent
-  ],
+  declarations: [AppComponent, ApartmentsListComponent, ApartmentComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
